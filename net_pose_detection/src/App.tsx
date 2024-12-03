4b3df07409e7e79b3e8e34025c86322f2f9df63d
VinayProgram <vinaytandale@gmail.com>
2024-12-03 11:05:26 +0530

    single pose detection

---
+++ net_pose_detection/src/App.tsx
@@ -0,0 +1,11 @@
+import * as poseDetection from '@tensorflow-models/pose-detection';
+import * as tf from '@tensorflow/tfjs-core';
+// Register one of the TF.js backends.
+const App = () => {
+  
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
